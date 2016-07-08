5a434b71966a02b1478a5d28f9433f2a510d2b51
Dum, Eric <edum@jrthompson.com>
2016-07-08 11:47:58 -0400

    Updated Layout
    Updated layout to be tile based. Used established behance user so that
    we could fill out layout.

--- assets/js/global/main.js
+++ assets/js/global/main.js
@@ -12,12 +12,16 @@ function loadProject(projectID) {
         moduleDescription += "<p>" + loadModules.description + "</p>";
         $(".modal-description").html(moduleDescription);
 
+        var moduleMeta = "";
+        moduleMeta += "<ul class='list-unstyled list-inline'><li><i class='fa fa-eye'></i> " + loadModules.stats.views + "</li><li><i class='fa fa-thumbs-up'></i> " + loadModules.stats.appreciations + "</li></ul>";
+        $(".modal-meta").html(moduleMeta);
+
         for(var i=0; i<loadModules.modules.length; i++) {
             if(loadModules.modules[i].src!=undefined) {
                 moduleHTML += "<p><img class='img-responsive' src='" + loadModules.modules[i].src + "'>";
                 moduleHTML += "</p>";
             }
-        }        
+        }
         $(".modal-work").html(moduleHTML);
         $('#myModal').modal('show');
     });
@@ -27,29 +31,37 @@ $(document).ready(function() {
 
     // Set behance api key
     behanceAPI.setKey(behancekey);
-    
+
     // Get projects
     behanceAPI.getProjects(user, function(projectlist) {
 
         //console.dir(projectlist);
-        
+
         var projectHTML = "";
-        
+
         for(var i=0; i<projectlist.length; i++) {
             var project = projectlist[i];
-            
+
             if (project.covers[1200] == undefined) {
                 imgSrc = project.covers[202];
-            } 
-            else {                
+            }
+            else {
                 imgSrc = project.covers[1200];
             }
-            
-            projectHTML +=  " <div class='col-lg-4 col-md-4 col-sm-4 col-xs-6 single-projects'> <h3> "+ project.name +"</h3> <a href='#'  title='"+ project.name +"' onclick='loadProject(" + project.id + ")'><img class='coverimage' src='" + imgSrc + "' class='col-lg-4'></a> </div>";
+
+            projectHTML +=
+                " <div class='col-lg-2 col-md-4 col-sm-4 col-xs-6 single-projects'>
+                    <a class='overlay' href='#'  title='"+ project.name +"' onclick='loadProject(" + project.id + ")'>
+                        <h3 class='project-title'> "+ project.name +"</h3>
+                        <hr>
+                        <h4 class='project-stats'>View Project</h4>
+                    </a>
+                    <img class='coverimage' src='" + imgSrc + "' alt='" + project.name + "'>
+                </div>";
         }
 
         projectHTML += "<div class='clearfix'></div>";
-        
+
         $("#projects").html(projectHTML);
     });
 }); //document.ready
